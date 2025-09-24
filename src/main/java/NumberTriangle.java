dbc96426997ddcf4800c67f977a91b2ffe21c5c1
wyattwong-git <wongwyattl@gmail.com>
2025-09-23 20:08:52 -0400

    Load Triangle

--- src/main/java/NumberTriangle.java
+++ src/main/java/NumberTriangle.java
@@ -1,4 +1,6 @@
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * This is the provided NumberTriangle class to be used in this coding task.
@@ -109,8 +111,7 @@ public class NumberTriangle {
         InputStream inputStream = NumberTriangle.class.getClassLoader().getResourceAsStream(fname);
         BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
 
-
-        // TODO define any variables that you want to use to store things
+        List<NumberTriangle> triangle = new ArrayList<>();
 
         // will need to return the top of the NumberTriangle,
         // so might want a variable for that.
@@ -122,7 +123,14 @@ public class NumberTriangle {
             // remove when done; this line is included so running starter code prints the contents of the file
             System.out.println(line);
 
-            // TODO process the line
+            String[] items = line.split(" ");
+
+            for (String item : items) {
+                NumberTriangle number = new NumberTriangle(Integer.parseInt(item));
+                triangle.add(number);
+            }
+
+            top = triangle.get(0);
 
             //read the next line
             line = br.readLine();
